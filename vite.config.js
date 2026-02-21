bd503e6feafc06e43255a72efdf695b5f828dcb4
Alex <apr45@njit.edu>
2026-02-20 20:50:37 -0500

    implementing config so css will load on live site

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Replace 'repo-name' with your actual repository name
+  base: '/is117-Assignment5/', 
+  build: {
+    outDir: 'dist',
+  }
+})
